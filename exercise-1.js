3f0a3f97a8e01202a8661f2447b3f71033890f49
alantheandroid <alessio.ilardi@gmail.com>
2022-05-14 18:50:59 +0200

    feat: add if/else-if statements

--- exercise-1.js
+++ exercise-1.js
@@ -1,20 +1,31 @@
-// ceo => 2200
-// manager => 1800
-// cto => 1800
-// developer => 1500
-// default => 1000
+let ceo = 2200;
+let manager = 1800;
+let cto = 1800;
+let developer = 1500;
+let other = 1000;
+
 function calculateSalary(role) {
-  // ...
+  if (role === "ceo") {
+    return ceo;
+  } else if (role === "manager") {
+    return manager;
+  } else if (role === "cto") {
+    return cto;
+  } else if (role === "developer") {
+    return developer;
+  } else if (role === "other") {
+    return other;
+  }
 }
 
-const ceoSalary = calculateSalary('ceo');
-const managerSalary = calculateSalary('manager');
-const ctoSalary = calculateSalary('cto');
-const developerSalary = calculateSalary('developer');
-const otherSalary = calculateSalary('other');
+const ceoSalary = calculateSalary("ceo");
+const managerSalary = calculateSalary("manager");
+const ctoSalary = calculateSalary("cto");
+const developerSalary = calculateSalary("developer");
+const otherSalary = calculateSalary("other");
 
-console.log(ceoSalary);
-console.log(managerSalary);
-console.log(ctoSalary);
-console.log(developerSalary);
-console.log(otherSalary);
+console.log("ceo =>", ceoSalary);
+console.log("manager =>", managerSalary);
+console.log("cto =>", ctoSalary);
+console.log("developer =>", developerSalary);
+console.log("other =>", otherSalary);
